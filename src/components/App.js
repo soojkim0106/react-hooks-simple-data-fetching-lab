5afa054b77f3cf13e03470171d3a770850b9cc59
Soojungkim <soojung.kimm@gmail.com>
2024-02-25 16:20:38 -0800

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("The json server is not running");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setImage(data.message);
+      });
+  }, []);
+
+  if (!image) {
+    return <p>Loading...</p>;
+  }
+  return <img src={image} alt="A Random Dog"></img>;
+};
+
+export default App;
